d1f662e6c552741328683a7966c7e8dbe02cba0b
choissoo99 <choissoo99@gmail.com>
2026-09-21 00:02:34 +0900

    Expose map markers for filtering

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL="https://zshtlatitnwcovkxzftv.supabase.co";
 const SUPABASE_KEY="sb_publishable_srMFnmLm1bCg9evU_X3Blw_T9vTcQwH";
-const map=L.map("map",{worldCopyJump:true}).setView([20,0],2);
+const map=L.map("map",{worldCopyJump:true}).setView([20,0],2);\nwindow.eventMarkers=[];
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{maxZoom:18,attribution:"&copy; OpenStreetMap"}).addTo(map);
 const esc=s=>String(s??"").replace(/[&<>"']/g,c=>({"&":"&amp;","<":"&lt;",">":"&gt;",'"':"&quot;","'":"&#39;"}[c]));
 async function loadEvents(){
